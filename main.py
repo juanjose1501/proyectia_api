be01bcb77e121da1a9dc407bffcbfd411cee0ba8
juanjose1501 <juanjose1514@gmail.com>
2020-12-13 23:08:24 -0500

    actualizacion frontend

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ import db.proyecto_db
 from models.proyecto_models import ProyectoIn, ProyectoOut
 from db import proyecto_db
 
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080",
+]
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
 
 @api.get("/proyecto/consulta/{nombre}")
 async def get_nombre(nombre: str):
